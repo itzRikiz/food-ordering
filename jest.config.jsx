ecbef5607ca9a2b7dd4ab7bc2c5f75769e1cc30f
itzRikiz <pain.sanmay@gmail.com>
2024-08-27 21:36:11 +0530

    test

---
+++ jest.config.jsx
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+};
